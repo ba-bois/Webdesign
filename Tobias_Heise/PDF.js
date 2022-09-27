461d0ed67ca566cdd6abf85507ee9b36b78da18b
TobiasHeise <tobias.heise01@web.de>
2022-09-27 02:13:18 +0200

    Done#2

--- Tobias_Heise/PDF.js
+++ Tobias_Heise/PDF.js
@@ -1,6 +1,6 @@
 window.jsPDF = window.jspdf.jsPDF;
 
-const formularelement = document.getElementById("1234").elements;
+const formularelement = document.getElementById("formu").elements;
 console.log(formularelement)
 const doc = new jsPDF()
 loremipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id eros turpis. Vivamus tempor urna vitae sapien mollis molestie. Vestibulum in lectus non enim bibendum laoreet at at libero. Etiam malesuada erat sed sem blandit in varius orci porttitor. Sed at sapien urna. Fusce augue ipsum, molestie et adipiscing at, varius quis enim. Morbi sed magna est, vel vestibulum urna. Sed tempor ipsum vel mi pretium at elementum urna tempor. Nulla faucibus consectetur felis, elementum venenatis mi mollis gravida. Aliquam mi ante, accumsan eu tempus vitae, viverra quis justo.\n\nProin feugiat augue in augue rhoncus eu cursus tellus laoreet. Pellentesque eu sapien at diam porttitor venenatis nec vitae velit. Donec ultrices volutpat lectus eget vehicula. Nam eu erat mi, in pulvinar eros. Mauris viverra porta orci, et vehicula lectus sagittis id. Nullam at magna vitae nunc fringilla posuere. Duis volutpat malesuada ornare. Nulla in eros metus. Vivamus a posuere libero.'
